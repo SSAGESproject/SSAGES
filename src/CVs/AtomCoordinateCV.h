a4d786b79a9742d967a876c59fb15262fe4c97fb
Jonathan K. Whitmer <jwhitme1@nd.edu>
2016-01-27 13:04:03 -0500

    Coordinate OP
    Suitable for my test system [2d particle] or Vik's Membrane PMF.

---
+++ src/CVs/AtomCoordinateCV.h
@@ -0,0 +1,82 @@
+#pragma once 
+
+#include "CollectiveVariable.h"
+
+#include <array>
+#include <math.h>
+
+namespace SSAGES
+{
+	class AtomCoordinateCV : public CollectiveVariable
+	{
+	private:
+		int _atomid; 
+		int _index;
+		double _val;
+		std::vector<Vector3> _grad;
+		std::array<double, 2> _bounds;
+
+		double norm(const Vector3& v)
+		{
+			return sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+		}
+
+	public:
+		AtomCoordinateCV(int atomid, int index) : 
+		_atomid(atomid), _index(index), _val(0), _grad(0), _bounds{{0,0}}
+		{
+		}
+
+		void Initialize(const Snapshot& snapshot) override
+		{
+			// Initialize gradient. 
+			auto n = snapshot.GetPositions().size();		
+			_grad.resize(n);
+		}
+
+		void Evaluate(const Snapshot& snapshot) override
+		{
+			// Gradient and value. 
+			auto& pos = snapshot.GetPositions(); 
+			auto& ids = snapshot.GetAtomIDs();
+			for(size_t i = 0; i < pos.size(); ++i)
+			{
+				if(ids[i] == _atomid)
+				{
+				  //not the most efficient way, but it does what it is supposed to in a minimal number of lines
+				  //grab positions, decide which one of the three directions matters, and move onward.
+					Vector3 dx{{pos[i][0], pos[i][1], pos[i][2]}};
+					for( jj = 0; jj < 3; jj++)
+					  if( jj != _index ) dx[jj] = 0;
+					
+					auto r = dx;
+					_grad[i][0] = dx[0]/r;
+					_grad[i][1] = dx[1]/r;
+					_grad[i][2] = dx[2]/r;
+					_val = r;
+				}
+				else
+				{
+					_grad[i][0] = 0;
+					_grad[i][1] = 0;
+					_grad[i][2] = 0;
+				}
+			}
+		}
+
+		double GetValue() const override 
+		{ 
+			return _val; 
+		}
+
+		const std::vector<Vector3>& GetGradient() const override
+		{
+			return _grad;
+		}
+
+		const std::array<double, 2>& GetBoundaries() const override
+		{
+			return _bounds;
+		}
+	};
+}
