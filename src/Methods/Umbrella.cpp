d6d985fed49a9eca3e22c1eb700031b9d3bea4b5
Ben S <bsikora906@gmail.com>
2016-04-18 13:00:28 -0400

    Update umbrella method to work with periodic CVs

--- src/Methods/Umbrella.cpp
+++ src/Methods/Umbrella.cpp
@@ -31,7 +31,7 @@ namespace SSAGES
 
 			// Compute dV/dCV.
 			auto D = _kspring[i]*(cv->GetDifference(_centers[i]));
-
+			
 			// Update forces.
 			for(size_t j = 0; j < forces.size(); ++j)
 				for(size_t k = 0; k < forces[j].size(); ++k)
