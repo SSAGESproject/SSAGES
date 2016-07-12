e32fe82f67ad37463de90671f2f8dfadd2b0b673
Cody Thomas Bezik <bezik@midway-login2.rcc.local>
2016-07-12 13:49:26 -0500

    Swarm updates + temporary user examples

--- src/Methods/Swarm.cpp
+++ src/Methods/Swarm.cpp
@@ -1,2 +1,171 @@
 #include "Swarm.h"
 #include "../spline.h"
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+
+namespace mpi = boost::mpi;
+namespace SSAGES
+{
+    //Pre-simulation hook
+    void Swarm::PreSimuation(Snapshot* snapshot, const CVList& cvs)
+    {
+        //Open file for writing
+        _mpiid = snapshot->GetWalkerID();
+        auto& positions = snapshot->GetPositions();
+        char file[1024];
+        sprintf(file, "node-%04.d.log", _mpiid);
+        _stringout.open(file);
+
+        //Sizing vectors
+        _worldstring.resize(_centers.size()); 
+        _cv_start.resize(_centers.size();
+        _cv_drift.resize(_centers.size());
+        _traj_positions.resize(_number_trajectories);
+        _traj_forces.resize(_number_trajectories);
+
+        //Initialize vector values
+        for(size_t i = 0; i < _centers.size(); i++)
+        {
+            _worldstring[i].resize(_numnodes);
+            _cv_prev[i] = cvs[i]->GetValue();
+
+            //_worldstring is indexed as cv followed by node
+            mpi::all_gather(_world, _centers[i], _worldstring[i]);
+
+            _cv_start[i] = 0;
+            _cv_drift[i] = 0; 
+        }
+
+        index = 0;
+    }
+
+    void Swarm::PostIntegration(Snapshot* snapshot, const CVList& cvs)
+    {
+        auto& forces = snapshot->GetForces();
+        auto& positions = snapshot->GetPositions();
+        int index; //For building the trajectory vectors
+
+        if(_iterator <= _initialize_steps + _restrained_steps)
+        {
+            //Do restrained sampling, and do not harvest trajectories
+            for(size_t i = 0; i < cvs.size(); i++)
+            {
+                if(_iterator = 0)
+                {
+                    index = 0; //Reset index when starting
+                }
+                //Get current CV and gradient
+                auto& cv = cvs[i];
+                auto& grad  cv->GetGradient();
+
+                //Compute dV/dCV
+                auto D = _spring*(cv->GetDifference(_centers[i]));
+
+                //Update forces
+                for(size_t j = 0; j < forces.size(), j++)
+                {
+                    for(size_t k = 0; k < forces[j].size(), k++)
+                    {
+                        forces[j][k] -= D*grad[j][k]; 
+
+                    }
+                }
+            }
+            if(_iterator > _initialize_steps)
+            {
+                //Harvest a trajectory every ten steps
+                if(_iterator % 10 == 0)
+                {
+                    _traj_positions[index];
+                    _traj_forces[index]; 
+                    index++;
+                }
+            }
+            _iterator++;
+        }
+        else if(_iterator <= _initialize_steps + _restrained_steps + _unrestrained_steps*_number_trajectories)
+        {
+            //Launch unrestrained trajectories
+            if(_iterator == _initialize_steps + _restrained_steps + 1)
+            {
+                index = 0; //Reset index when starting unrestrained trajectories
+            }
+            if((iterator - _initialize_steps - restrained_steps) % unrestrained_steps == 1)
+            {
+                //Start of trajectory, reset positions and forces
+                forces = _traj_forces[index];
+                positions = _traj_positions[index];
+                index++;
+
+                //Record CV starting values
+                for(size_t i = 0; i < _cv_start.size(); i++)
+                {
+                    _cv_start[i] = cvs[i]; 
+                }
+            }
+
+            if((_iterator - _initialize_steps - _restrained_steps) % _unrestrained_steps == 0)
+            {
+                //End of trajectory, harvest drift
+                for(size_t i = 0; i < _cv_drift.size(); i++)
+                {
+                    _cv_drift[i] += cvs[i] - _cv_start[i]; //Add up drifts, average later
+                }
+            }
+            _iterator++;
+        }
+        else
+        {
+            //Average drift
+            for(size_t i = 0; i < _cv_drift.size(); i++)
+            {
+                _cv_drift[i] /= _number_trajectories;
+            }
+
+            //Evolve CVs, reparametrize, and reset vectors
+            _currentiter++;
+
+            StringUpdate();
+
+            PrintString(cvs);
+
+            _iterator == 0;
+
+            for(size_t i = 0; i < _cv_drift.size(); i++)
+            {
+                _cv_drift = 0; 
+            }
+        }
+    }
+
+    //Post simulation hook
+    void Swarm::PostSimulation(Snapshot*, const CVList&)
+    {
+        _stringout.close();
+    }
+
+    void Swarm::PrintString(const CVList& CV)
+    {
+        _stringout.precision(8);
+        _stringout << _mpiid << " " << _currentiter << " ";
+
+        for(size_t i = 0; i < _centers.size(); i++)
+        {
+            _stringout << _centers[i] << " " << CV[i]->GetValue() << " ";
+        }
+        _stringout << std::endl;
+
+        std::cout << _mpiid < " " << _currentiter << " ";
+        for(size_t i = 0; i < _centers.size(); i++)
+        {
+            std::cout << _centers[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void Swarm::StringUpdate()
+    {
+
+    }
+}
--- src/Methods/Swarm.h
+++ src/Methods/Swarm.h
@@ -22,6 +22,9 @@ namespace SSAGES
             //! Number of nodes on a string
             unsigned int _numnodes;
 
+            //! Length of initialization
+            unsigned int _initialize_steps; 
+
             //! Length of restrained sampling 
             unsigned int _restrained_steps; 
 
@@ -29,6 +32,68 @@ namespace SSAGES
             unsigned int _number_trajectories;
 
             //! Length of unrestrained trajectories
-            unsigned int _unrestrained_steps;
-    }
+            unsigned int _unrestrained_steps; 
+
+            //! The local method iterator
+            unsigned int _iterator;
+
+            //! CV starting location values
+            std::vector<double> _centers; 
+
+            //! Drift of CVs for one iteration
+            std::vector<double> _cv_drift; 
+
+            //! Start of CV values for a trajectory
+            std::vector<double> _cv_start;
+
+            //! Mesh index value for reparametrization
+            double _alpha; 
+
+            //! Index of all CV values on the string
+            std::vector<std::vector<double>> _worldstring;
+
+            //! Umbrella strength
+            double _spring; 
+
+            //! For indexing trajectory vectors
+            int _index; 
+
+            //! Output stream for string data
+            std::ofstream _stringout; 
+
+            //! Updates the positions of the string
+            void StringUpdate();
+
+            //! Prints the string to a file
+            void PrintString(const CVList& CV);
+
+        public:
+
+            //! Constructor
+            /*!
+             * Constructs an instance of the swarm of trajectories method.
+             */
+            Swarm(boost::mpi::communicator& world, boost::mpi::communicator& com, const std::vector<double>& centers, unsigned int NumNodes, double spring, unsigned int frequency, unsigned int InitialSteps, unsigned int RestrainedSteps, unsigned int NumberTrajectories, unsigned int UnrestrainedSteps) : Method(frequency, world, com) _centers(centers), _cv_prev(), alpha(), _mpiid(0), _worldstring(), _currentiter(0), _initiaize_steps(InitialSteps), _restrained_steps(RestrainedSteps), _number_trajectories(NumberTrajectories), _unrestrained_steps(UnrestrainedSteps), _iterator(0); 
+
+            //! Pre-simulation hook
+            void PreSimulation(Snapshot* snapshot, const CVList& cvs) override;
+
+            //! Post-integration hook
+            void PostIntegration(Snapshot* snapshot, const CVList& cvs) override; 
+
+            //! Post-simulation hook
+            void PostSimulation(Snapshot* snapshot, const CVList& cvs) override; 
+
+            void Serialize(Json::Value& json) const override; 
+            {
+
+            }
+
+            //! Destructor
+            ~Swarm()
+            {
+
+            }
+    };
 }
+
