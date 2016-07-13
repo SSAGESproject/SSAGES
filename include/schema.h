f8673633ffd4c843d975ce151143e0d6df0af48e
Cody Thomas Bezik <bezik@depablo-login1.voth.local>
2016-07-13 17:35:51 -0500

    Fix schema update

--- include/schema.h
+++ include/schema.h
@@ -12,36 +12,21 @@ namespace SSAGES
 		static std::string Simulation;
 		static std::string driver;
 		static std::string LAMMPSDriver;
-<<<<<<< HEAD
 		static std::string TorsionalCV;
 		static std::string ImproperCV;
 		static std::string AtomSeparationCV;
 		static std::string AtomPositionCV;
 		static std::string AtomCoordinateCV;
 		static std::string CVs;
+		static std::string UmbrellaMethod;
 		static std::string SwarmMethod;
-		static std::string UmbrellaMethod;
 		static std::string methods;
 		static std::string MetadynamicsMethod;
 		static std::string FTSMethod;
 		static std::string ForwardFluxMethod;
 		static std::string ElasticBandMethod;
-=======
-		static std::string CVs;
-		static std::string AtomCoordinateCV;
-		static std::string ImproperCV;
-		static std::string AtomSeparationCV;
-		static std::string AtomPositionCV;
-		static std::string TorsionalCV;
 		static std::string BFSMethod;
-		static std::string MetadynamicsMethod;
->>>>>>> refs/remotes/WhitmerGroup/Development
 		static std::string ABFMethod;
-		static std::string methods;
-		static std::string UmbrellaMethod;
-		static std::string ForwardFluxMethod;
-		static std::string ElasticBandMethod;
-		static std::string FTSMethod;
 		
 	};
 }
