905faa67d202c23319067c8645c25b7006394354
Julian Helfferich <julian.helfferich@googlemail.com>
2016-08-11 11:02:06 -0500

    Unit Test for UnwrapCoordinates (#109)
    The unit test covers particles in the box and outside the box for
    a cubic, an orthorombic, a monclinic and a triclinic box.
    
    Implementing the unit test, I have realized that you can actually
    give fractional values for the box images. This should be changed
    and only take integers (we need a IntVector3 for this).

--- src/Utility/UnwrapCoordinates.h
+++ src/Utility/UnwrapCoordinates.h
@@ -1,48 +1,62 @@
 #pragma once
 
 #include "UnitCellConversion.h"
-
+#include "types.h"
 
 namespace SSAGES
 {
-	//! Helper function to unwrap coordinates.
-	/*!
-	 * \param LatticeConstants Lattice Constants (a, b, c, alpha, beta, gamma).
-	 * \param image Mirror images in the three dimensions.
-	 * \param coord Coordinates.
-	 *
-	 * \return Vector3 containing the unwrapped coordinates.
-	 *
-	 * Converts Cartesian to fractional, adjusts the fractional coordinates
-	 * to appropriate images, then converts back to Cartesian.
-	 *
-	 * \ingroup Utility
-	 */
-	Vector3 UnwrapCoordinates(const std::array<double, 6>& LatticeConstants, const Vector3& image, const Vector3& coord)
+    //! Helper function to unwrap coordinates.
+    /*!
+     * \param LatticeConstants Lattice Constants (a, b, c, alpha, beta, gamma).
+     * \param coord Coordinates.
+     * \param image Mirror images in the three dimensions.
+     *
+     * \return Vector3 containing the unwrapped coordinates.
+     *
+     * This function takes a set of (wrapped) coordinates and the information in
+     * which mirror box the atom resides in. It returns the unwrapped atom
+     * position. This is achieved by converting the Cartesian coordinates to
+     * fractional coordinates, adjusting the fractional coordinates to the
+     * appropriate mirror box, and then converting back to Cartesian coordinates.
+     *
+     * \note This function does not require the initial coordinates to be within
+     *       the simulation box.
+     *
+     * \note This function assumes that the simulation box is periodic in all
+     *       three dimensions.
+     *
+     * \note The values for the mirror images can take fractional values. The
+     *       use of fractional images is highly discouraged and will be made
+     *       invalid soon.
+     *
+     * \ingroup Utility
+     */
+    Vector3 UnwrapCoordinates(const std::array<double, 6>& LatticeConstants,
+                              const Vector3& coord,
+                              const Vector3& image)
+    {
+        auto c2f = uc_c2f(LatticeConstants);
+        auto f2c = uc_f2c(LatticeConstants);
 
-		//double unwrap_coord(const Vector3& coord, Vector3& image, Vector3& b, Vector3& a)
-		{
+        // Convert Cartesian to fractional coordinates
+        Vector3 fract(
+            c2f[0][0]*(coord[0]) + c2f[0][1]*(coord[1]) + c2f[0][2]*(coord[2]),
+            c2f[1][1]*(coord[1]) + c2f[1][2]*(coord[2]),
+            c2f[2][2]*(coord[2])
+        );
 
-			auto c2f = uc_c2f(LatticeConstants);
-			auto f2c = uc_f2c(LatticeConstants);
+        // Add pertinent images
+        Vector3 un_fract(fract[0] + image[0],
+                         fract[1] + image[1],
+                         fract[2] + image[2]);
 
-		// Convert Cartesian to fractional coordinates
-			Vector3 fract_coord{{c2f[0][0]*(coord[0]]) + c2f[0][1]*(coord[1]) + c2f[0][2]*(coord[2])
-							,c2f[1][1]*(coord[1]) + c2f[1][2]*(coord[2])
-							,c2f[2][2]*(coord[2]) }};
+        // Convert back to cartesian coordinates
+        Vector3 un_coord(
+            f2c[0][0]*un_fract[0] + f2c[0][1]*un_fract[1] + f2c[0][2]*un_fract[2],
+            f2c[1][1]*un_fract[1] + f2c[1][2]*un_fract[2],
+            f2c[2][2]*un_fract[2]
+        );
 
-		// Add pertinent images
-
-			Vector3 un_fract_coord{{fract_coord[0] + image[0],
-									fract_coord[1] + image[1],
-									fract_coord[2] + image[2]}};
-		// Convert back to cartesian coordinates
-			Vector3 un_coord{{f2c[0][0]*un_fract_coord[0] + f2c[0][1]*un_fract_coord[1] + f2c[0][2]*un_fract_coord[2]
-						,f2c[1][1]*un_fract_coord[1] + f2c[1][2]*un_fract_coord[2]
-						,f2c[2][2]*un_fract_coord[2]}};
-
-			return un_coord
-
-
-		}
+        return un_coord;
+    }
 }
