fc37cb1de55072fb0e866f2aeb1c88b10d3e301a
Hythem Sidky <sidky@outlook.com>
2017-04-07 17:40:47 -0400

    Initial new driver.

---
+++ src/NewDriver.cpp
@@ -0,0 +1,29 @@
+/**
+ * This file is part of
+ * SSAGES - Suite for Advanced Generalized Ensemble Simulations
+ *
+ * Copyright 2017 Hythem Sidky <hsidky@nd.edu>
+ *
+ * SSAGES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SSAGES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SSAGES.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "NewDriver.h" 
+
+namespace SSAGES
+{
+    NewDriver::NewDriver(const MPI_Comm& world, const MPI_Comm& comm, uint walkerid) : 
+    world_(world), comm_(comm), walkerid_(walkerid)
+    {
+        
+    }
+}
---
+++ src/NewDriver.h
@@ -0,0 +1,68 @@
+/**
+ * This file is part of
+ * SSAGES - Suite for Advanced Generalized Ensemble Simulations
+ *
+ * Copyright 2017 Hythem Sidky <hsidky@nd.edu>
+ *
+ * SSAGES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SSAGES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SSAGES.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once 
+
+#include "JSON/Serializable.h"
+#include <mpi.h>
+
+namespace SSAGES
+{
+    //! Primary abstract class that drives a simulation. 
+    /*!
+     * 
+     * Driver is responsible for intialzing the major components 
+     * of a simulation including the engine, methods, and collective
+     * variables. It is also responsible for the lifetime of the objects 
+     * it creates. 
+     * 
+     * Each simulation engine must implement a concrete driver which 
+     * handles the implementation-specific parts of the initialization 
+     * routines.
+     */
+    class NewDriver : public Serializable
+    {
+    protected: 
+        //! MPI communicator containing all processors. 
+        MPI_Comm world_; 
+
+        //! MPI communicator containing processors for specific walker. 
+        MPI_Comm comm_;
+
+        //! Walker ID for specific driver. 
+        uint walkerid_; 
+
+        //! Snapshot of system state (pointer). 
+        class Snapshot* snapshot_;
+
+        //! Vector of advanced sampling methods. 
+        std::vector<class Method*> methods_;
+
+        //! Collective variable manager. 
+        class CVManager cvmanager_;
+    public:
+        //! Constructor. 
+        /*!
+         * \param world MPI communicator containing all processors. 
+         * \param comm MPI communicator containing walker-specific processors. 
+         * \param walkerid ID of the walker for the current processor. 
+         */
+        NewDriver(const MPI_Comm& world, const MPI_Comm& comm, uint walkerid);
+    };
+}
