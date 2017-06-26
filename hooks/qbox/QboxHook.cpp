e298c7d062162e2345432b48a562cdedbd26c75f
Hythem Sidky <sidky@outlook.com>
2017-06-26 12:46:16 -0400

    Removed boost from qbox and updated driver.

--- hooks/qbox/QboxHook.cpp
+++ hooks/qbox/QboxHook.cpp
@@ -1,27 +1,11 @@
 #include "QboxHook.h"
 #include "Snapshot.h"
-#include <boost/property_tree/xml_parser.hpp>
-#include <boost/lexical_cast.hpp>
-#include <boost/algorithm/string/trim.hpp>
+#include "pugixml.hpp"
 #include <exception>
+#include <fstream>
 
 namespace SSAGES
 {
-	template<typename T>
-	std::vector<T> to_vector(std::string s)
-	{
-		using boost::algorithm::trim;
-		trim(s);
-
-		std::vector<T> result;
-		std::stringstream ss(s);
-		std::string item;
-		while(std::getline(ss, item, ' ')) 
-			result.push_back(boost::lexical_cast<T>(item));
-		
-		return result;
-	}
-
 	template<typename T>
 	void to_vector3(const std::string& s, T& vec)
 	{
@@ -34,15 +18,12 @@ namespace SSAGES
 		std::ofstream fout;
 		fout.open(commandfile, std::ofstream::trunc);
 		
-		auto& atomset = pt_.get_child("fpmd:simulation.iteration.atomset");
-		for(const auto& v : atomset)
+		auto atomset = xml_.child("fpmd:simulation").child("iteration").child("atomset");
+		for(auto& atom : atomset.children("atom"))
 		{
-			if(v.first == "atom")
-			{
-				auto name = v.second.get<std::string>("<xmlattr>.name");
-				fout << "extforce define atomic " 
-				     << name << " " << name << " 0 0 0 " << std::endl;
-			}
+			auto name = atom.attribute("name").value();
+			fout << "extforce define atomic " 
+					<< name << " " << name << " 0 0 0 " << std::endl;
 		}
 
 		fout.close();		
@@ -51,15 +32,10 @@ namespace SSAGES
 	void QboxHook::XMLToSSAGES(const std::string& xmlfile)
 	{
 		// Load the XML file into property tree. 
-		try {
-			read_xml(xmlfile, pt_);
-		} catch(std::exception& e) {
-			// If it fails, force filesystem flush and try again.
-			sync();
-			usleep(100000);
-			read_xml(xmlfile, pt_);
-		}
-
+		auto result = xml_.load_file(xmlfile.c_str()); 
+		if(!result)
+			throw std::runtime_error(result.description());
+		
 		SyncToSnapshot();
 	}
 
@@ -75,33 +51,19 @@ namespace SSAGES
 		const auto& vel = snapshot_->GetVelocities();
 		const auto& frc = snapshot_->GetForces();
 		const auto& typ = snapshot_->GetAtomTypes();
-
-		auto& atomset = pt_.get_child("fpmd:simulation.iteration.atomset");
+		
+		auto atomset = xml_.child("fpmd:simulation").child("iteration").child("atomset");
 
 		int i = 0;
-		for(const auto& v : atomset)
+		for(auto& atom : atomset.children("atom"))
 		{
-			if(v.first == "atom")
-			{
-				auto name = v.second.get<std::string>("<xmlattr>.name");
-				
-				/*fout << "move " << name << " to " 
-				     << pos[i][0] << " "
-				     << pos[i][1] << " "
-				     << pos[i][2] << std::endl;
+			auto name = atom.attribute("name").value();
+			fout << "extforce set " << name << " "
+					<< frc[i][0] - prevforces_[i][0] << " "
+					<< frc[i][1] - prevforces_[i][1] << " "
+					<< frc[i][2] - prevforces_[i][2] << std::endl;
 
-				fout << "set_velocity " << name << " " 
-				     << vel[i][0] << " "
-				     << vel[i][1] << " "
-				     << vel[i][2] << std::endl;*/
-
-				fout << "extforce set " << name << " "
-				     << frc[i][0] - prevforces_[i][0] << " "
-				     << frc[i][1] - prevforces_[i][1] << " "
-				     << frc[i][2] - prevforces_[i][2] << std::endl;
-
-				++i;
-			}
+			++i;
 		}
 	}
 
@@ -111,29 +73,26 @@ namespace SSAGES
 		speciesmass_.clear();
 		species_.reserve(32);
 		speciesmass_.reserve(32);
-
-		auto& species = pt_.get_child("fpmd:simulation");
-		for(auto& s : species)
+		
+		auto species = xml_.child("fpmd:simulation");
+		for(auto& s : species.children("species"))
 		{
-			if(s.first == "species")
-			{
-				species_.push_back(s.second.get<std::string>("<xmlattr>.name"));
-				speciesmass_.push_back(s.second.get<double>("mass"));
-			}
+			species_.push_back(s.attribute("name").value());
+			speciesmass_.push_back(std::atof(s.child_value("mass")));
 		}
 	}
 
 	void QboxHook::SyncToSnapshot()
 	{
-		auto& atomset = pt_.get_child("fpmd:simulation.iteration.atomset");
+		auto atomset = xml_.child("fpmd:simulation").child("iteration").child("atomset");
 
 		// First entry in <atomset> is <unit_cell>.
-		int natoms = atomset.size() - 1;
+		int natoms = std::distance(atomset.children("atom").begin(), atomset.children("atom").end());
 
 		// Build species index on first iteration. 
 		if(snapshot_->GetIteration() == 0)
 			BuildSpeciesInfo();
-
+		
 		// Load em up. 
 		snapshot_->SetIteration(snapshot_->GetIteration() + 1);
 		snapshot_->SetNumAtoms(natoms);
@@ -161,38 +120,34 @@ namespace SSAGES
 		prevforces_.resize(natoms, Vector3{0,0,0});
 
 		int i = 0;
-		for(const auto& v : atomset)
+		for(auto& atom : atomset.children("atom"))
 		{
-			if(v.first == "atom")
-			{
-				to_vector3<Vector3>(v.second.get<std::string>("position"), pos[i]);
-				to_vector3<Vector3>(v.second.get<std::string>("velocity"), vel[i]);
-				to_vector3<Vector3>(v.second.get<std::string>("force"), frc[i]);
-				prevforces_[i] = frc[i]; // Store "previous forces".
-				ids[i] = i + 1;
+			to_vector3<Vector3>(atom.child_value("position"), pos[i]);
+			to_vector3<Vector3>(atom.child_value("velocity"), vel[i]);
+			to_vector3<Vector3>(atom.child_value("force"), frc[i]);
+			prevforces_[i] = frc[i]; // Store "previous forces".
+			ids[i] = i + 1;
+			
+			// Get species types by resolving map. 
+			int type = GetSpeciesIndex(atom.attribute("species").value());
+			if(type == -1)
+				throw std::logic_error("Invalid atom type resolved at index " + std::to_string(i));
 
-				// Get species types by resolving map. 
-				int type = GetSpeciesIndex(v.second.get<std::string>("<xmlattr>.species"));
-				if(type == -1)
-					throw std::logic_error("Invalid atom type resolved at index " + std::to_string(i));
-
-				typs[i] = type;
-				mass[i] = speciesmass_[type];
-				++i;
-			} 
-			else if(v.first == "unit_cell")
-			{
-				Matrix3 H; 
-				Vector3 Hi; 
-				to_vector3<Vector3>(v.second.get<std::string>("<xmlattr>.a"), Hi);
-				H.row(0) = Hi;
-				to_vector3<Vector3>(v.second.get<std::string>("<xmlattr>.b"), Hi);
-				H.row(1) = Hi;
-				to_vector3<Vector3>(v.second.get<std::string>("<xmlattr>.c"), Hi);
-				H.row(2) = Hi;
-				snapshot_->SetHMatrix(H);
-			}
+			typs[i] = type;
+			mass[i] = speciesmass_[type];
+			++i;
 		}
+		
+		Matrix3 H; 
+		Vector3 Hi; 
+		auto box = atomset.child("unit_cell");
+		to_vector3<Vector3>(box.attribute("a").value(), Hi);
+		H.row(0) = Hi;
+		to_vector3<Vector3>(box.attribute("b").value(), Hi);
+		H.row(1) = Hi;
+		to_vector3<Vector3>(box.attribute("c").value(), Hi);
+		H.row(2) = Hi;
+		snapshot_->SetHMatrix(H);
 	}
 
 	void QboxHook::SyncToEngine()
