19e44865b484b21114856230e3b592196f544698
hadi-ramezani <hadi.ramezani@gmail.com>
2017-07-12 22:02:06 -0500

    Started updating FFS for the new json input.

--- Examples/User/ForwardFlux/LAMMPS/Langevin/FF_Input_Generator.py
+++ Examples/User/ForwardFlux/LAMMPS/Langevin/FF_Input_Generator.py
@@ -1,29 +1,28 @@
 #! /usr/bin/env python
 
 # This file is used to take a template input json file for forward flux
-# and create a input file for ssages that uses multiple drivers.
+# and create a input file for ssages that uses multiple walkers.
 
 import json 
 import numpy as np
-import copy
 from random import randint
 
 class LessPrecise(float):
     def __repr__(self):
         return str(self)
 
-def lammps_random_number(lammps_filename, nDrivers):
+def lammps_random_number(lammps_filename, nWalkers):
     """Takes a lammps input file, finds the seed number of langevin, replaces it with a random number, and
 	generates new input files
 	"""
     randon_numbers = set()
-    while (len(randon_numbers) < nDrivers):
+    while (len(randon_numbers) < nWalkers):
         randon_numbers.add(randint(0, 100000))
     randon_numbers = list(randon_numbers)
 
     in_lammps_file = open(lammps_filename, 'r')
 
-    for i in range(0, nDrivers):
+    for i in range(0, nWalkers):
         out_lammps_file = open(lammps_filename + '-' + str(i), 'w')
         for line in in_lammps_file:
             if line.find('langevin') != -1:
@@ -42,12 +41,15 @@ def lammps_random_number(lammps_filename, nDrivers):
 
 
 # User must set these variables
-nDrivers = 2
-lammps_random_number("in.LAMMPS_FF_Test_1d", nDrivers)
+nWalkers = 2
+input_filename = "in.LAMMPS_FF_Test_1d"
 interfaces = np.array([-1.0, -0.95, -0.8, 0, 1])
 trials = np.empty(5, dtype=int)
 trials.fill(50)
 # Use interfaces = np.linspace(<firstInterface>, <lastInterface>, num=<nInterfaces>) if you have many equally-spaced interfaces
+
+#Generate the new lammps input files
+lammps_random_number(input_filename, nWalkers)
 
 # Open template and load in the json data. 
 root = {}
@@ -55,19 +57,15 @@ with open('Template_Input.json') as f:
     root = json.load(f)
 
 
-#print interfaces
-root['driver'][0]['method']['interfaces'] = interfaces.tolist()
-root['driver'][0]['method']['trials'] = trials.tolist()
+root["walkers"] = nWalkers
+root['methods'][0]['interfaces'] = interfaces.tolist()
+root['methods'][0]['trials'] = trials.tolist()
 
-# Add on the requested number of objects -1 because we are appending
-for i in range(0, nDrivers):
-    root['driver'].append(copy.deepcopy(root['driver'][0]))
-    # Change the input filename so each driver uses a different input file file
-    root['driver'][i]['inputfile'] = root['driver'][i]['inputfile'] + '-' + str(i)
+input = []
+for i in range(0, nWalkers):
+    input.append(input_filename+ '-' + str(i))
+root['input'] = input
 	
-# Because appending remove original copy
-root['driver'] = root['driver'][1:]
-
 # Convert python dictionary into JSON file
-with open('Input-2proc.json', 'w') as f:
+with open('Input-2walkers.json', 'w') as f:
         json.dump(root, f, indent=4, separators=(',', ': '))
