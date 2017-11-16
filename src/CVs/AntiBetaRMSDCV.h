ba28d8274c47e872a88dcf4f5fc6caece79029d7
Ashley Guo <azguo@uchicago.edu>
2017-11-15 18:01:23 -0600

    correct antibeta sscv for multiple proc per walker
    also get rid of mysterious dos newlines???

--- src/CVs/AntiBetaRMSDCV.h
+++ src/CVs/AntiBetaRMSDCV.h
@@ -47,21 +47,23 @@ namespace SSAGES
 		//! Residue IDs for secondary structure calculation
 		std::vector<int> resids_;
 
-		//! Atom IDs for secondary structure calculation: backbone of resids_
-		//std::vector<int> atomids_;
+		//! Atom IDs and chain IDs for secondary structure calculation: backbone of resids_
 		std::vector< std::vector<std::string> > atomids_;
+
+		//! Atom IDs only, for secondary structure calculation: backbone atoms of resids_
+		std::vector<int> ids_only_;
 
 		//! Name of pdb reference for system
 		std::string refpdb_;
-
-		//! Coordinates for reference structure
-		std::vector<Vector3> refalpha_;
 
 		//! Length unit conversion: convert 1 nm to your internal MD units (ex. if using angstroms use 10)
 		double unitconv_;
 
 		//! Specify whether to calculate beta sheet character in intra or inter mode: 0 for either, 1 for inter, 2 for intra
 		int mode_;
+
+		//! Pairwise distance between backbone atoms for reference structure.
+		std::vector<double> refdists_; 
 
 	public:
 		//! Constructor.
@@ -104,39 +106,65 @@ namespace SSAGES
 		void Initialize(const Snapshot& snapshot) override
 		{
 			atomids_ = ReadBackbone::GetPdbBackbone(refpdb_, resids_);
+			ids_only_.resize(atomids_[0].size());
+
+			// check to find all necessary atoms
+			std::transform(atomids_[0].begin(), atomids_[0].end(), ids_only_.begin(), [](std::string val) {return std::stoi(val);});
+			using std::to_string;
+			std::vector<int> found;
+			snapshot.GetLocalIndices(ids_only_, &found);
+			int nfound = found.size();
+			MPI_Allreduce(MPI_IN_PLACE, &nfound, 1, MPI_INT, MPI_SUM, snapshot.GetCommunicator());
+			if(nfound != resids_.size() * 5)
+				throw BuildException({
+					"OHOHOoohoho: Expected to find " +
+					to_string(resids_.size() * 5) +
+					" atoms, but only found " +
+					to_string(nfound) + "."
+				});
 
 			// reference 'ideal' antiparallel beta sheet structure in nanometers
 			// Reference structure taken from values used in Plumed 2.2.0
-			refalpha_.push_back(unitconv_ * Vector3{ .2263, -.3795,  .1722}); // N    i
-			refalpha_.push_back(unitconv_ * Vector3{ .2493, -.2426,  .2263}); // CA
-			refalpha_.push_back(unitconv_ * Vector3{ .3847, -.1838,  .1761}); // CB
-			refalpha_.push_back(unitconv_ * Vector3{ .1301, -.1517,  .1921}); // C
-			refalpha_.push_back(unitconv_ * Vector3{ .0852, -.1504,  .0739}); // O
-			refalpha_.push_back(unitconv_ * Vector3{ .0818, -.0738,  .2917}); // N    i+1
-			refalpha_.push_back(unitconv_ * Vector3{-.0299,  .0243,  .2748}); // CA
-			refalpha_.push_back(unitconv_ * Vector3{-.1421, -.0076,  .3757}); // CB
-			refalpha_.push_back(unitconv_ * Vector3{ .0273,  .1680,  .2854}); // C
-			refalpha_.push_back(unitconv_ * Vector3{ .0902,  .1993,  .3888}); // O
-			refalpha_.push_back(unitconv_ * Vector3{ .0119,  .2532,  .1813}); // N    i+2
-			refalpha_.push_back(unitconv_ * Vector3{ .0683,  .3916,  .1680}); // CA
-			refalpha_.push_back(unitconv_ * Vector3{ .1580,  .3940,  .0395}); // CB
-			refalpha_.push_back(unitconv_ * Vector3{-.0394,  .5011,  .1630}); // C
-			refalpha_.push_back(unitconv_ * Vector3{-.1459,  .4814,  .0982}); // O
-			refalpha_.push_back(unitconv_ * Vector3{-.2962,  .3559, -.1359}); // N    j - 2
-			refalpha_.push_back(unitconv_ * Vector3{-.2439,  .2526, -.2287}); // CA
-			refalpha_.push_back(unitconv_ * Vector3{-.1189,  .3006, -.3087}); // CB
-			refalpha_.push_back(unitconv_ * Vector3{-.2081,  .1231, -.1520}); // C
-			refalpha_.push_back(unitconv_ * Vector3{-.1524,  .1324, -.0409}); // O
-			refalpha_.push_back(unitconv_ * Vector3{-.2326,  .0037, -.2095}); // N    j - 1
-			refalpha_.push_back(unitconv_ * Vector3{-.1858, -.1269, -.1554}); // CA
-			refalpha_.push_back(unitconv_ * Vector3{-.3053, -.2199, -.1291}); // CB
-			refalpha_.push_back(unitconv_ * Vector3{-.0869, -.1949, -.2512}); // C
-			refalpha_.push_back(unitconv_ * Vector3{-.1255, -.2070, -.3710}); // O
-			refalpha_.push_back(unitconv_ * Vector3{ .0326, -.2363, -.2072}); // N    j
-			refalpha_.push_back(unitconv_ * Vector3{ .1405, -.2992, -.2872}); // CA
-			refalpha_.push_back(unitconv_ * Vector3{ .2699, -.2129, -.2917}); // CB
-			refalpha_.push_back(unitconv_ * Vector3{ .1745, -.4399, -.2330}); // C
-			refalpha_.push_back(unitconv_ * Vector3{ .1899, -.4545, -.1102}); // O
+			std::vector<Vector3> refalpha;
+			refalpha.push_back(unitconv_ * Vector3{ .2263, -.3795,  .1722}); // N    i
+			refalpha.push_back(unitconv_ * Vector3{ .2493, -.2426,  .2263}); // CA
+			refalpha.push_back(unitconv_ * Vector3{ .3847, -.1838,  .1761}); // CB
+			refalpha.push_back(unitconv_ * Vector3{ .1301, -.1517,  .1921}); // C
+			refalpha.push_back(unitconv_ * Vector3{ .0852, -.1504,  .0739}); // O
+			refalpha.push_back(unitconv_ * Vector3{ .0818, -.0738,  .2917}); // N    i+1
+			refalpha.push_back(unitconv_ * Vector3{-.0299,  .0243,  .2748}); // CA
+			refalpha.push_back(unitconv_ * Vector3{-.1421, -.0076,  .3757}); // CB
+			refalpha.push_back(unitconv_ * Vector3{ .0273,  .1680,  .2854}); // C
+			refalpha.push_back(unitconv_ * Vector3{ .0902,  .1993,  .3888}); // O
+			refalpha.push_back(unitconv_ * Vector3{ .0119,  .2532,  .1813}); // N    i+2
+			refalpha.push_back(unitconv_ * Vector3{ .0683,  .3916,  .1680}); // CA
+			refalpha.push_back(unitconv_ * Vector3{ .1580,  .3940,  .0395}); // CB
+			refalpha.push_back(unitconv_ * Vector3{-.0394,  .5011,  .1630}); // C
+			refalpha.push_back(unitconv_ * Vector3{-.1459,  .4814,  .0982}); // O
+			refalpha.push_back(unitconv_ * Vector3{-.2962,  .3559, -.1359}); // N    j - 2
+			refalpha.push_back(unitconv_ * Vector3{-.2439,  .2526, -.2287}); // CA
+			refalpha.push_back(unitconv_ * Vector3{-.1189,  .3006, -.3087}); // CB
+			refalpha.push_back(unitconv_ * Vector3{-.2081,  .1231, -.1520}); // C
+			refalpha.push_back(unitconv_ * Vector3{-.1524,  .1324, -.0409}); // O
+			refalpha.push_back(unitconv_ * Vector3{-.2326,  .0037, -.2095}); // N    j - 1
+			refalpha.push_back(unitconv_ * Vector3{-.1858, -.1269, -.1554}); // CA
+			refalpha.push_back(unitconv_ * Vector3{-.3053, -.2199, -.1291}); // CB
+			refalpha.push_back(unitconv_ * Vector3{-.0869, -.1949, -.2512}); // C
+			refalpha.push_back(unitconv_ * Vector3{-.1255, -.2070, -.3710}); // O
+			refalpha.push_back(unitconv_ * Vector3{ .0326, -.2363, -.2072}); // N    j
+			refalpha.push_back(unitconv_ * Vector3{ .1405, -.2992, -.2872}); // CA
+			refalpha.push_back(unitconv_ * Vector3{ .2699, -.2129, -.2917}); // CB
+			refalpha.push_back(unitconv_ * Vector3{ .1745, -.4399, -.2330}); // C
+			refalpha.push_back(unitconv_ * Vector3{ .1899, -.4545, -.1102}); // O
+
+			// calculate all necessary pairwise distances for reference structure
+			std::fill(refdists_.begin(), refdists_.end(), 0.0);
+			refdists_.resize(900, 0.0);
+			for(size_t k = 0; k < 29; k++){
+				for(size_t h = k + 1; h < 30; h++){
+					refdists_[29 * k + h] = (refalpha[k] - refalpha[h]).norm();
+				}
+			}
 		}
 
 		//! Evaluate the CV.
@@ -145,19 +173,16 @@ namespace SSAGES
 		 */
 		void Evaluate(const Snapshot& snapshot) override
 		{
-			// need atom positions for all atoms in atomids_
+			// need atom positions for all atoms
 			const auto& pos = snapshot.GetPositions();
-			std::vector<int> groupidx;
-			std::vector<int> double_atomids(atomids_[0].size());
-			std::transform(atomids_[0].begin(), atomids_[0].end(), double_atomids.begin(), [](std::string val) {return std::stod(val);});
-			snapshot.GetLocalIndices(double_atomids, &groupidx);
+			auto& comm = snapshot.GetCommunicator();
 
 			unsigned int resgroups = resids_.size() - 2;
 			double rmsd, dist_norm, dxgrouprmsd;
+			unsigned int localidx;
 			Vector3 dist_xyz, dist_ref;
 			std::vector<Vector3> refxyz;
 			std::vector< std::vector< Vector3 > > deriv(30, std::vector<Vector3>(30, Vector3{0,0,0}));
-
 			std::fill(grad_.begin(), grad_.end(), Vector3{0,0,0});
 			grad_.resize(snapshot.GetNumAtoms(), Vector3{0,0,0});
 			val_ = 0.0;
@@ -169,19 +194,24 @@ namespace SSAGES
 						rmsd = 0.0;
 						std::fill(refxyz.begin(), refxyz.end(), Vector3{0,0,0});
 						refxyz.resize(30, Vector3{0,0,0});
+
 						for(size_t k = 0; k < 15; k++){
-							refxyz[k] = pos[groupidx[5 * i + k]];
+							localidx = snapshot.GetLocalIndex(ids_only_[5 * i + k]);
+							if(localidx != -1) refxyz[k] = pos[localidx];
 						}
+
 						for(size_t k = 0; k < 15; k++){
-							refxyz[k + 15] = pos[groupidx[5 * j + k]];
+							localidx = snapshot.GetLocalIndex(ids_only_[5 * j + k]);
+							if(localidx != -1) refxyz[k + 15] = pos[localidx];
 						}
+
+						MPI_Allreduce(MPI_IN_PLACE, refxyz.data(), 90, MPI_DOUBLE, MPI_SUM, comm);
 
 						// sum over all pairs to calculate CV
 						for(size_t k = 0; k < 29; k++){
 							for(size_t h = k + 1; h < 30; h++){
 								dist_xyz = refxyz[k] - refxyz[h];
-								dist_ref = refalpha_[k] - refalpha_[h];
-								dist_norm = dist_xyz.norm() - dist_ref.norm();
+								dist_norm = dist_xyz.norm() - refdists_[29 * k + h];
 								rmsd += dist_norm * dist_norm;
 								deriv[k][h] = dist_xyz * dist_norm / dist_xyz.norm();
 							}
@@ -197,18 +227,24 @@ namespace SSAGES
 
 						for(size_t k = 0; k < 15; k++){
 							for(size_t h = k + 1; h < 15; h++){
-								grad_[groupidx[5 * i + k]] += dxgrouprmsd * deriv[k][h];
-								grad_[groupidx[5 * i + h]] -= dxgrouprmsd * deriv[k][h];
+								localidx = snapshot.GetLocalIndex(ids_only_[5 * i + k]);
+								if(localidx != -1) grad_[localidx] += dxgrouprmsd * deriv[k][h];
+								localidx = snapshot.GetLocalIndex(ids_only_[5 * i + h]);
+								if (localidx != -1) grad_[localidx] += dxgrouprmsd * deriv[k][h];
 							}
 							for(size_t h = 0; h < 15; h++){
-								grad_[groupidx[5 * i + k]] += dxgrouprmsd * deriv[k][h+15];
-								grad_[groupidx[5 * j + h]] -= dxgrouprmsd * deriv[k][h+15];
+								localidx = snapshot.GetLocalIndex(ids_only_[5 * i + k]);
+								if(localidx != -1) grad_[localidx] += dxgrouprmsd * deriv[k][h + 15];
+								localidx = snapshot.GetLocalIndex(ids_only_[5 * j + h]);
+								if(localidx != -1) grad_[localidx] += dxgrouprmsd * deriv[k][h + 15];
 							}
 						}
 						for(size_t k = 0; k < 14; k++){
 							for(size_t h = k + 1; h < 15; h++){
-								grad_[groupidx[5 * j + k]] += dxgrouprmsd * deriv[k+15][h+15];
-								grad_[groupidx[5 * j + h]] -= dxgrouprmsd * deriv[k+15][h+15];
+								localidx = snapshot.GetLocalIndex(ids_only_[5 * j + k]);
+								if(localidx != -1) grad_[localidx] += dxgrouprmsd * deriv[k + 15][h + 15];
+								localidx = snapshot.GetLocalIndex(ids_only_[5 * j + h]);
+								if(localidx != -1) grad_[localidx] += dxgrouprmsd * deriv[k + 15][h + 15];
 							}
 						}
 					}
