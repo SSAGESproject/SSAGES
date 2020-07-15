738f11d5f82f070f79d5ff3d580fa42ad8d3c5a2
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2020-07-15 16:45:37 -0500

    Fetch Eigen at build time instead of config time

--- cmake/Modules/FetchEigen.cmake
+++ cmake/Modules/FetchEigen.cmake
@@ -1,42 +1,24 @@
 set(EIGEN_REPOSITORY "https://gitlab.com/libeigen/eigen.git")
-set(EIGEN_PATCH "${CMAKE_CURRENT_SOURCE_DIR}/include/patches/eigen-using-std-real.patch")
 set(EIGEN_TAG 3.3.7)
+set(EIGEN_PATCH "${CMAKE_CURRENT_SOURCE_DIR}/include/patches/eigen-using-std-real.patch")
 
-if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/include/eigen")
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} clone --branch=${EIGEN_TAG} --depth=1
-            -c advice.detachedHead=false ${EIGEN_REPOSITORY}
-        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include"
-        RESULT_VARIABLE EIGEN_GIT_CLONE_RESULT
-    )
-    if(NOT EIGEN_GIT_CLONE_RESULT EQUAL "0")
-        message(FATAL_ERROR
-            "`git clone ${EIGEN_REPOSITORY}` failed with ${EIGEN_GIT_CLONE_RESULT}"
-        )
-    endif()
-endif()
-
-execute_process(
-    COMMAND ${GIT_EXECUTABLE} apply --check ${EIGEN_PATCH}
-    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include/eigen"
-    RESULT_VARIABLE EIGEN_GIT_PATCH_RESULT
+include(ExternalProject)
+ExternalProject_Add(eigen
+    # Using DOWNLOAD_COMMAND instead of GIT_REPOSITORY to pass some convenient git flags.
+    # For instance, GIT_SHALLOW is available from CMake 3.6
+    DOWNLOAD_COMMAND ${GIT_EXECUTABLE} clone --branch=${EIGEN_TAG} --depth=1
+        -c advice.detachedHead=false ${EIGEN_REPOSITORY}
+    PATCH_COMMAND ${GIT_EXECUTABLE} apply ${EIGEN_PATCH}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+    LOG_UPDATE ON
 )
-if(EIGEN_GIT_PATCH_RESULT EQUAL "0")
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} apply ${EIGEN_PATCH}
-        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include/eigen"
-    )
-else()
-    message(WARNING
-        "`git apply --check ${EIGEN_PATCH}` failed with ${EIGEN_GIT_PATCH_RESULT}"
-    )
-endif()
 
-if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/include/Eigen")
-    file(
-        COPY "${CMAKE_CURRENT_SOURCE_DIR}/include/eigen/Eigen"
-        DESTINATION "${CMAKE_CURRENT_SOURCE_DIR}/include"
-    )
-endif()
-
-file(REMOVE_RECURSE "${CMAKE_CURRENT_SOURCE_DIR}/include/eigen")
+ExternalProject_Add_Step(eigen copy_eigen
+    COMMAND ${CMAKE_COMMAND} -E copy_directory
+        <SOURCE_DIR>/Eigen ${CMAKE_CURRENT_SOURCE_DIR}/include/Eigen
+    DEPENDEES update
+    DEPENDERS configure
+)
