c28ce126f0c977f48523007fac844797b0e5fe0e
Michael Quevillon <mquevill@nd.edu>
2020-08-20 10:50:53 -0400

    Add support for HOOMD 2.6 and newer (#572)
    * Update HOOMD Hook for more current versions
    
    * Update HOOMD compilation instructions
    
    * Leave future versions of  HOOMD support open-ended (minimum 2.4.0)
    
    SSAGES will *most likely* work with newer versions of HOOMD, unless there is an API change (like there was in 2.6.0).

--- hooks/hoomd/FindHOOMD.cmake
+++ hooks/hoomd/FindHOOMD.cmake
@@ -14,17 +14,48 @@
 # as a convenience (for the intended purpose of this find script), all include directories and definitions needed
 # to compile with all the various libs (boost, python, winsoc, etc...) are set within this script
 
+set(HOOMD_ROOT "" CACHE FILEPATH "Directory containing a hoomd installation (i.e. _hoomd.so)")
+
+# Let HOOMD_ROOT take precedence, but if unset, try letting Python find a hoomd package in its default paths.
 if(HOOMD_ROOT)
-  message(STATUS "Using HOOMD installation at " ${HOOMD_ROOT})
+    set(hoomd_installation_guess ${HOOMD_ROOT})
 else(HOOMD_ROOT)
-  message(FATAL_ERROR "HOOMD_ROOT must be set to the HOOMD installation path.")
+    find_package(PythonInterp)
+
+    set(find_hoomd_script "
+from __future__ import print_function;
+import sys, os; sys.stdout = open(os.devnull, 'w')
+import hoomd
+print(os.path.dirname(hoomd.__file__), file=sys.stderr, end='')"
+    )
+
+    execute_process(
+        COMMAND ${PYTHON_EXECUTABLE} -c "${find_hoomd_script}"
+        ERROR_VARIABLE hoomd_installation_guess
+    )
+    message(STATUS "Python output: " ${hoomd_installation_guess})
 endif(HOOMD_ROOT)
 
+message(STATUS "Looking for a HOOMD installation at " ${hoomd_installation_guess})
+find_path(
+    FOUND_HOOMD_ROOT
+    NAMES _hoomd.so __init__.py
+    HINTS ${hoomd_installation_guess}
+)
+
+if(FOUND_HOOMD_ROOT)
+    set(HOOMD_ROOT ${FOUND_HOOMD_ROOT} CACHE FILEPATH "Directory containing a hoomd installation (i.e. _hoomd.so)" FORCE)
+    message(STATUS "Found hoomd installation at " ${HOOMD_ROOT})
+else(FOUND_HOOMD_ROOT)
+    message(FATAL_ERROR "Could not find hoomd installation, either set HOOMD_ROOT or set PYTHON_EXECUTABLE to a python which can find hoomd")
+endif(FOUND_HOOMD_ROOT)
+
 # search for the hoomd include directory
-find_path(HOOMD_INCLUDE_DIR
-          NAMES HOOMDVersion.h
-          HINTS ${HOOMD_ROOT}/include
-          )
+find_path(
+    HOOMD_INCLUDE_DIR
+    NAMES HOOMDVersion.h
+    HINTS ${HOOMD_ROOT}/include
+)
 
 if(HOOMD_INCLUDE_DIR)
     message(STATUS "Found HOOMD include directory: ${HOOMD_INCLUDE_DIR}")
@@ -34,17 +65,28 @@ endif(HOOMD_INCLUDE_DIR)
 
 set(FOUND_HOOMD TRUE)
 
+execute_process (
+    COMMAND "sed" "-n"
+    "s/\#define HOOMD_VERSION_\\(MAJOR\\|MINOR\\|PATCH\\) \\([0-9]\\+\\)/\\2/p"
+    "${HOOMD_INCLUDE_DIR}/HOOMDVersion.h"
+    COMMAND "sed" "-e" ":a" "-e" "N" "-e" "\$!ba" "-e" "s/\\n/./g"
+    OUTPUT_VARIABLE HOOMD_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
 #############################################################
 ## Now that we've found hoomd, lets do some setup
 
 include_directories(${HOOMD_INCLUDE_DIR})
 
 # run all of HOOMD's generic lib setup scripts
-set(CMAKE_MODULE_PATH ${HOOMD_ROOT}
-                      ${HOOMD_ROOT}/CMake/hoomd
-                      ${HOOMD_ROOT}/CMake/thrust
-                      ${CMAKE_MODULE_PATH}
-                      )
+set(
+    CMAKE_MODULE_PATH
+    ${HOOMD_ROOT}
+    ${HOOMD_ROOT}/CMake/hoomd
+    ${HOOMD_ROOT}/CMake/thrust
+    ${CMAKE_MODULE_PATH}
+)
 
 # grab previously-set hoomd configuration
 include (hoomd_cache)
