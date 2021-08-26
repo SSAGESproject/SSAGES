d5f4d1a7522d73cf9d2d827f277ce3227580d1ca
Michael Quevillon <mquevill@nd.edu>
2021-08-26 11:28:34 -0400

    Upgrade Eigen to 3.4.0 (#620)
    The std::real patch is no longer needed.

--- cmake/Modules/FetchEigen.cmake
+++ cmake/Modules/FetchEigen.cmake
@@ -1,6 +1,5 @@
 set(EIGEN_REPOSITORY "https://gitlab.com/libeigen/eigen.git")
-set(EIGEN_TAG 3.3.7)
-set(EIGEN_PATCH "${CMAKE_CURRENT_SOURCE_DIR}/include/patches/eigen-using-std-real.patch")
+set(EIGEN_TAG 3.4.0)
 
 ExternalProject_Add(eigen
     # Using DOWNLOAD_COMMAND instead of GIT_REPOSITORY to pass some convenient git flags.
@@ -8,7 +7,6 @@ ExternalProject_Add(eigen
     DOWNLOAD_COMMAND ${GIT_EXECUTABLE} clone --branch=${EIGEN_TAG} --depth=1
         -c advice.detachedHead=false ${EIGEN_REPOSITORY}
     UPDATE_COMMAND ""
-    PATCH_COMMAND ${GIT_EXECUTABLE} apply ${EIGEN_PATCH}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${CMAKE_COMMAND} -E copy_directory
         <SOURCE_DIR>/Eigen ${CMAKE_CURRENT_SOURCE_DIR}/include/Eigen
